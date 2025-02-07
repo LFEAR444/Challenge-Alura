18bf38d099dead877797e44e46d3a7a711948955
LFEAR444 <niklerlost@outlook.com>
2025-02-06 22:35:28 -0300

    avanze js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+//Crear un array para almacenar los nombres
+let amigos =[];
 
-let amigos = [];
+//Implementa una función para agregar amigos
+function agregarAmigo(){
+    let tpInput = document.getElementById("amigo").value;
+    if(tpInput === ""){
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(tpInput);
+        console.log(amigos);
+        document.getElementById("amigo").value = "";
+    }
+    
+}
+
